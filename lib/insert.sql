d5ff1697dc4238b38cbed7c3f0bc2c81a56eb811
Khalid Mabrux <abelmabruke@gmail.com>
2025-05-29 09:55:07 +0300

    Update insert.sql

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 7, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 10, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 6, 'F', 'Gray', 'Friendly', 1),
+('Sergeant Brown', 19, 'M', 'Brown', 'Stoic', 0),
+('Melissa', 13, 'F', 'Red', 'Playful', 1),
+('Grinch', 2, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 6, 'F', 'White', 'Happy', 1),
+(NULL, 4, 'M', 'Black', 'Mysterious', 0);
